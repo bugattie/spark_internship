b8e20d17a4cc6c518c6e5c42bdf21379d856ccb2
bugattie <m.mohsinbarkaati@gmail.com>
2021-03-04 12:48:09 +0500

    about.py for spark about page test cases

---
+++ task6/about.py
@@ -0,0 +1,32 @@
+from selenium import webdriver
+import unittest
+import page
+
+class SparkAboutPageTest(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+        self.driver.get('https://www.thesparksfoundationsingapore.org/about/advisors-and-patrons/')
+        self.driver.implicitly_wait(5)
+
+    # def test_logo(self):
+    #     about_page = page.AboutUsPage(self.driver)
+    #     assert about_page.is_logo_dispalyed()
+
+    # def test_navbar_appears(self):
+    #     about_page = page.AboutUsPage(self.driver)
+    #     assert about_page.navbar_appears()
+
+    def test_advisors_images(self):
+        about_page = page.AboutUsPage(self.driver)
+        assert about_page.check_advisors_image()
+
+    # def test_footer(self):
+    #     main_page = page.MainPage(self.driver)
+    #     assert main_page.check_footer()
+
+
+    def tearDown(self):
+        self.driver.close()
+
+if __name__ == '__main__':
+    unittest.main()
